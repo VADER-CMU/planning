a4320613bc79b3bf03cbd295492f9f5c7c7b150c
Tom Gao <zimingg2001@gmail.com>
2025-02-26 18:01:00 -0500

    Works when grabbing fruit with left arm and cutting from right

--- src/vader_planner/scripts/vader_pepper_publisher.py
+++ src/vader_planner/scripts/vader_pepper_publisher.py
@@ -29,26 +29,51 @@ def generate_solid_primitive():
 
 def generate_fruit():
     fruit = Fruit()
-    fruit.pose = generate_random_pose()
-    fruit.shape = generate_solid_primitive()
+    pose = Pose()
+    pose.position.x = random.uniform(0.4, 0.5)
+    pose.position.y = random.uniform(0.2, 0.3)
+    pose.position.z = 0.3#random.uniform(0.5, 0.8)
+    #Assume identity rotation for now
+    pose.orientation.x = 0
+    pose.orientation.y = 0
+    pose.orientation.z = 0
+    pose.orientation.w = 1
+    fruit.pose = pose
+    fruit_primitive = SolidPrimitive()
+    fruit_primitive.type = SolidPrimitive.CYLINDER
+    fruit_primitive.dimensions = [0.1, 0.075]
+    fruit.shape = fruit_primitive
     return fruit
 
-def generate_peduncle():
+def generate_peduncle(fruit):
     peduncle = Peduncle()
-    peduncle.pose = generate_random_pose()
-    peduncle.shape = generate_solid_primitive()
+
+    peduncle_primitive = SolidPrimitive()
+    peduncle_primitive.type = SolidPrimitive.CYLINDER
+    peduncle_primitive.dimensions = [0.1, 0.01]
+    pose = Pose()
+    pose.position.x = fruit.pose.position.x
+    pose.position.y = fruit.pose.position.y
+    pose.position.z = fruit.pose.position.z + fruit.shape.dimensions[0] / 2 + peduncle_primitive.dimensions[0] / 2
+    #Assume identity rotation for now
+    pose.orientation.x = 0
+    pose.orientation.y = 0
+    pose.orientation.z = 0
+    pose.orientation.w = 1
+    peduncle.pose = pose
+    peduncle.shape = peduncle_primitive
     return peduncle
 
 def generate_pepper():
     pepper = Pepper()
     pepper.fruit_data = generate_fruit()
-    pepper.peduncle_data = generate_peduncle()
+    pepper.peduncle_data = generate_peduncle(pepper.fruit_data)
     return pepper
 
 def publisher():
     rospy.init_node('vader_pepper_publisher', anonymous=True)
     pub = rospy.Publisher(PEPPER_TOPIC, Pepper, queue_size=10)
-    rate = rospy.Rate(0.5)  # once every 20 seconds
+    rate = rospy.Rate(0.1)  # once every 20 seconds
 
     rospy.sleep(1) # Wait for the publisher to be registered + everything launched
 
--- src/vader_planner/src/vader_PR2_planner.cpp
+++ src/vader_planner/src/vader_PR2_planner.cpp
@@ -4,17 +4,22 @@
  *
  * Author: Jason Peng <jason@ufactory.cc>
  ============================================================================*/
- #include <moveit/move_group_interface/move_group_interface.h>
- #include <moveit/planning_scene_interface/planning_scene_interface.h>
- 
- #include <moveit_msgs/DisplayRobotState.h>
- #include <moveit_msgs/DisplayTrajectory.h>
- 
- #include <moveit_msgs/AttachedCollisionObject.h>
- #include <moveit_msgs/CollisionObject.h>
- 
- #include <moveit_visual_tools/moveit_visual_tools.h>
- #include <std_msgs/Bool.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <std_msgs/Bool.h>
+
+#include <tf/transform_broadcaster.h>//include for quaternion rotation 
+#include <tf/transform_listener.h>//include for quaternion rotation 
+#include <tf/transform_datatypes.h>//include for quaternion rotation 
+#include <tf/tf.h>
 
 #include <vader_msgs/Pepper.h>
 #include <vader_msgs/Peduncle.h>
@@ -39,7 +44,15 @@
  class VADERPR2Planner
  {
    public:
-     VADERPR2Planner():spinner(SPINNER_THREAD_NUM){init();};
+     VADERPR2Planner():
+        spinner(SPINNER_THREAD_NUM),
+        PLANNING_GROUP_L(PLAN_GROUP_L_PREFIX),
+        PLANNING_GROUP_R(PLAN_GROUP_R_PREFIX),
+        group_L(PLAN_GROUP_L_PREFIX),
+        group_R(PLAN_GROUP_R_PREFIX)
+        {
+          init();
+        };
      ~VADERPR2Planner(){ delete visual_tools;};
      void do_pepper_plan(const vader_msgs::Pepper::ConstPtr& msg);
      bool gripper_plan_and_execute(const vader_msgs::Fruit& fruit_pose);
@@ -50,13 +63,14 @@
    private:
      ros::NodeHandle node_handle;
      ros::AsyncSpinner spinner;
-    //  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
     //  std::vector<std::string> joint_names;
-    //  moveit::planning_interface::MoveGroupInterface group;
-    //  moveit::planning_interface::MoveGroupInterface::Plan my_xarm_plan;
+     moveit::planning_interface::MoveGroupInterface group_L, group_R;
+     moveit::planning_interface::MoveGroupInterface::Plan plan_L, plan_R;
      moveit_visual_tools::MoveItVisualTools *visual_tools;
-    //  std::string left_or_right_arm;
-    //  std::string PLANNING_GROUP;
+
+    std::string PLANNING_GROUP_L;
+    std::string PLANNING_GROUP_R;
  
      ros::Publisher display_path;
      ros::Subscriber pepper_sub_;
@@ -70,16 +84,18 @@
  {
     display_path = node_handle.advertise<moveit_msgs::DisplayTrajectory>("move_group/display_planned_path", 1, true); /*necessary?*/
   
-    // visual_tools->publishText(text_pose, PLANNING_GROUP + "_xArm Planner Demo", rvt::WHITE, rvt::XLARGE);
-    // visual_tools->trigger();
+    visual_tools = new moveit_visual_tools::MoveItVisualTools("L_link_base");
+    Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+    text_pose.translation().z() = 0.8;
+
+    visual_tools->publishText(text_pose, "VADER PR2 Planner Demo", rvt::WHITE, rvt::XLARGE);
+    visual_tools->trigger();
     // std::cout << "VaderPlanner waiting for messages" << std::endl;
     // Initialize subscriber and publisher
     pepper_sub_ = node_handle.subscribe(PEPPER_SUBSCRIBE_TOPIC, 10, &VADERPR2Planner::do_pepper_plan, this);
-
-    visual_tools = new moveit_visual_tools::MoveItVisualTools("link_base");
-    Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
-    text_pose.translation().z() = 0.8;
- }
+    ROS_INFO("Planner initialized with left planning group: %s and right planning group: %s", \
+      PLANNING_GROUP_L.c_str(), PLANNING_GROUP_R.c_str());
+  }
  
  void VADERPR2Planner::start()
  {
@@ -96,42 +112,92 @@
  {
    if(plan_result)
    {
-    //  ROS_INFO_NAMED("xarm_planner", "Visualizing plan as trajectory line");
+     ROS_INFO_NAMED("xarm_planner", "Visualizing plan as trajectory line");
      
-    //  visual_tools->deleteAllMarkers();
-    //  const robot_state::JointModelGroup* joint_model_group = group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-    //  visual_tools->publishTrajectoryLine(my_xarm_plan.trajectory_, joint_model_group);
-    //  visual_tools->trigger();
+     visual_tools->deleteAllMarkers();
+     const robot_state::JointModelGroup* joint_model_group_L = group_L.getCurrentState()->getJointModelGroup(PLANNING_GROUP_L);
+     visual_tools->publishTrajectoryLine(plan_L.trajectory_, joint_model_group_L);
+     const robot_state::JointModelGroup* joint_model_group_R = group_R.getCurrentState()->getJointModelGroup(PLANNING_GROUP_R);
+     visual_tools->publishTrajectoryLine(plan_R.trajectory_, joint_model_group_R);
+     visual_tools->trigger();
    }
  }
 
+ //Rotates given pose by 90 deg around the y axis
+geometry_msgs::Pose calculateFruitPregraspPose(const geometry_msgs::Pose& target_pose) {
+  // Assuming target_pose contains the necessary information to calculate the pregrasp pose
+  tf::Quaternion pregrasp_pose;
 
- geometry_msgs::Pose calculateFruitPOI(const vader_msgs::Pepper::ConstPtr& msg) {
-  // Assuming msg contains the necessary information to calculate the POI
-  geometry_msgs::Pose pose_msg;
-  // Fill in pose_msg based on msg TODO Rohit
-  return pose_msg;
-}
+  tf::Quaternion rotation_quat;
+  rotation_quat.setRPY( -M_PI/2,0, 0);
 
- geometry_msgs::Pose calculatePedunclePOI(const vader_msgs::Pepper::ConstPtr& msg) {
-  // Assuming msg contains the necessary information to calculate the POI
-  geometry_msgs::Pose pose_msg;
-  // Fill in pose_msg based on msg TODO Rohit
-  return pose_msg;
-}
+  tf::Quaternion original_quat;
+  tf::quaternionMsgToTF(target_pose.orientation, original_quat);
+
+  pregrasp_pose = rotation_quat * original_quat;
+  pregrasp_pose.normalize();
+
+  geometry_msgs::Pose pregrasp_pose_result;
+  tf::quaternionTFToMsg(pregrasp_pose, pregrasp_pose_result.orientation);
+  
+  pregrasp_pose_result.position.x = target_pose.position.x;// + 0.1; // Adjust as needed
+  pregrasp_pose_result.position.y = target_pose.position.y;// + 0.1; // Adjust as needed
+  pregrasp_pose_result.position.z = target_pose.position.z;// + 0.1; // Adjust as needed
+  return pregrasp_pose_result;
+ }
+ 
+geometry_msgs::Pose calculatePedunclePregraspPose(const geometry_msgs::Pose& target_pose) {
+  // Assuming target_pose contains the necessary information to calculate the pregrasp pose
+  tf::Quaternion pregrasp_pose;
+
+  tf::Quaternion rotation_quat;
+  rotation_quat.setRPY( M_PI/2,0, 0);
+
+  tf::Quaternion original_quat;
+  tf::quaternionMsgToTF(target_pose.orientation, original_quat);
+
+  pregrasp_pose = rotation_quat * original_quat;
+  pregrasp_pose.normalize();
+
+  geometry_msgs::Pose pregrasp_pose_result;
+  tf::quaternionTFToMsg(pregrasp_pose, pregrasp_pose_result.orientation);
+  
+  pregrasp_pose_result.position.x = target_pose.position.x;// + 0.1; // Adjust as needed
+  pregrasp_pose_result.position.y = target_pose.position.y;// + 0.1; // Adjust as needed
+  pregrasp_pose_result.position.z = target_pose.position.z;// + 0.1; // Adjust as needed
+  return pregrasp_pose_result;
+ }
 
  void VADERPR2Planner::do_pepper_plan(const vader_msgs::Pepper::ConstPtr& msg) {
-  // std::cout << "Hello" << std::endl;  
-  ROS_INFO_NAMED("do_pepper_plan", "Received new pepper planning request");
+    // std::cout << "Hello" << std::endl;  
+    ROS_INFO_NAMED("do_pepper_plan", "Received new pepper planning request");
+
+    vader_msgs::Fruit fruit = msg->fruit_data;
+    vader_msgs::Peduncle peduncle = msg->peduncle_data;
+    //Make it a collision object
+    moveit_msgs::CollisionObject fruit_collision_obj;
+    fruit_collision_obj.id = "fruit_1";
+    fruit_collision_obj.header.frame_id = "L_link_base";
+    fruit_collision_obj.primitives.push_back(fruit.shape);
+    fruit_collision_obj.primitive_poses.push_back(fruit.pose);
+    fruit_collision_obj.operation = fruit_collision_obj.ADD;
+    planning_scene_interface.applyCollisionObject(fruit_collision_obj);
+    //Make it a collision object
+    moveit_msgs::CollisionObject peduncle_collision_obj;
+    peduncle_collision_obj.id = "peduncle_1";
+    peduncle_collision_obj.header.frame_id = "L_link_base";
+    peduncle_collision_obj.primitives.push_back(peduncle.shape);
+    peduncle_collision_obj.primitive_poses.push_back(peduncle.pose);
+    peduncle_collision_obj.operation = peduncle_collision_obj.ADD;
+    planning_scene_interface.applyCollisionObject(peduncle_collision_obj);
 
     // Do gripper movement
-    vader_msgs::Fruit fruit = msg->fruit_data;
     bool gripper_success = VADERPR2Planner::gripper_plan_and_execute(fruit);
-
+    // bool gripper_success = true;
     ROS_INFO_NAMED("do_pepper_plan", "Gripper planning and execution completed");
 
     // Do cutter movement
-    vader_msgs::Peduncle peduncle = msg->peduncle_data;
+    
     bool cutter_success = VADERPR2Planner::cutter_plan_and_execute(peduncle);
 
     // Conclude
@@ -141,22 +207,47 @@
     // std::cout << "Done" << std::endl;
  }
 
- bool VADERPR2Planner::gripper_plan_and_execute(const vader_msgs::Fruit& fruit_pose) {
+ bool VADERPR2Planner::gripper_plan_and_execute(const vader_msgs::Fruit& fruit) {
     ROS_INFO_NAMED("gripper_plan_and_execute", "Received new gripper planning request");
-    // TODO: Abhi: Add collision object for CUTTER ARM
-    //plan path to pose
-    //execute path
-    return true; //success
+    geometry_msgs::Pose target_pose = calculateFruitPregraspPose(fruit.pose);
+    target_pose.position.x = target_pose.position.x;// fruit.shape.dimensions[fruit.shape.CYLINDER_RADIUS];
+    target_pose.position.y = target_pose.position.y - fruit.shape.dimensions[fruit.shape.CYLINDER_RADIUS];
+    target_pose.position.z = target_pose.position.z;// + fruit.shape.dimensions[fruit.shape.CYLINDER_RADIUS];
+
+    group_L.setPoseTarget(target_pose);
+    
+    bool success = (group_L.plan(plan_L) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    if (!success) {
+      ROS_ERROR_NAMED("gripper_plan_and_execute", "Failed to plan for gripper");
+      return false;
+    }
+    show_trail(success);
+    bool exec_ok = (group_L.execute(plan_L) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    return exec_ok;
  }
 
- bool VADERPR2Planner::cutter_plan_and_execute(const vader_msgs::Peduncle& peduncle_pose) {
+ bool VADERPR2Planner::cutter_plan_and_execute(const vader_msgs::Peduncle& peduncle) {
     ROS_INFO("Received new cutter planning request");
-    // TODO: Abhi: Add collision object for GRIPPER ARM
-    //plan path to pose
-    //execute path
-    return true; //success
+    geometry_msgs::Pose target_pose = calculatePedunclePregraspPose(peduncle.pose);
+    target_pose.position.x = target_pose.position.x;//peduncle.shape.dimensions[peduncle.shape.CYLINDER_RADIUS];
+    target_pose.position.y = target_pose.position.y + peduncle.shape.dimensions[peduncle.shape.CYLINDER_RADIUS];// + 0.05;
+    target_pose.position.z = target_pose.position.z + peduncle.shape.dimensions[peduncle.shape.CYLINDER_HEIGHT]/ 3;
+
+    // TODO: Abhi: Add collision object for CUTTER ARM
+
+    group_R.setPoseTarget(target_pose);
+    
+    bool success = (group_R.plan(plan_R) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    if (!success) {
+      ROS_ERROR_NAMED("gripper_plan_and_execute", "Failed to plan for gripper");
+      return false;
+    }
+    show_trail(success);
+    bool exec_ok = (group_R.execute(plan_R) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    return exec_ok;
  }
 
+ 
  
  int main(int argc, char** argv)
  {
